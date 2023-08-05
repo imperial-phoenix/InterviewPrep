9c0b62b78eaac919bcd0c36bb4bdeaf2ddc62ae4
n.zinoviev <zinoviev.sd@gmail.com>
2023-08-05 18:53:16 +0300

    Добавлена функция для вычисления корней квадратного уравнения

--- SimpleTasks/main.cpp
+++ SimpleTasks/main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include <algorithm>
+#include <cmath>
+
+
+/**
+* @brief Решение квадратного уравнения.
+*/
+void
+SolveQuadraticEquation()
+{
+   /*
+    * Алгоритм решения квадратного уравнения:
+    * 1) Проверить является ли уравнение квадратным.
+    * 2) Вычислить дискриминант D квадратного уравнения.
+    * 3) Если D < 0, то уравнение не имеет корней.
+    * 4) Если D = 0, то уравнение имеет два одинаковых корня.
+    * 5) Если D > 0, то уравнение имеет два корня.
+   */  
+
+   double a, b, c;
+   std::cin >> a >> b >> c;
+
+   if (0 == a) // линейное уравнение Bx + C = 0
+   {
+      if (b != 0) // Bx = -C => x = -C / B
+      {
+         std::cout << -c / b << std::endl;
+      }
+      else
+      {
+         //std::cout << "Уравнение не имеет корней." << std::endl;
+      }
+   }
+   else // квадратное уравнение
+   {
+      double d = b * b - 4 * a * c;
+
+      if (0 == d) // уравнение имеет один корень
+      {
+         std::cout << -b / (2 * a) << std::endl;
+      }
+      else if (d > 0)
+      {
+         std::cout << (-b + std::sqrt(d)) / (2 * a) <<
+               " " << (-b - std::sqrt(d)) / (2 * a) << std::endl;   
+      }
+      else
+      {
+         //std::cout << "Уравнение не имеет действительных корней." << std::endl;
+      }
+   }
+   
+}
 
 
 int main()
 {
-   std::cout << "Hello, world!\n";
-   
+   SolveQuadraticEquation();
+
    return 0;
 }
