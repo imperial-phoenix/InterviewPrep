c0d43a4ff30829b04cdb4ed91415e1466d0f63b5
n.zinoviev <zinoviev.sd@gmail.com>
2023-11-08 18:23:53 +0300

    Добавлены псевдонимы типов для класса Array.

--- Src/Array/Array.hpp
+++ Src/Array/Array.hpp
@@ -4,6 +4,30 @@
 #ifndef __ARRAY_H__
 #define __ARRAY_H__
 
+namespace Containers {
+
+template <typename T, std::size_t N>
+class Array
+{
+public:
+   using value_type        = T;
+   using size_type         = std::size_t;
+   using difference_type   = std::ptrdiff_t;
+   using reference         = value_type&;
+   using const_reference   = const value_type&;
+   using pointer           = value_type*;
+   using const_pointer     = const value_type*;
+
+   operator[](size_type Index)
+   {
+      return data_[Index];
+   }
+
+
+   T data_[N];
+};
+
+}
 
 
 #endif // __ARRAY_H__
--- Src/Array/Test/ArrayTest.cpp
+++ Src/Array/Test/ArrayTest.cpp
@@ -1,11 +1,23 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 
+#include <array>
 #include <iostream>
 
 #include "gtest/gtest.h"
 
 #include "Array.hpp"
+
+
+TEST(ArrayConstructorTest, ImplicitCtor)
+{
+   /*** Arrange ***/
+   // Containers::Array<int, 5> array{1, 2, 3, 4, 5};
+
+   /*** Act ***/
+
+   /*** Assert ***/
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////
