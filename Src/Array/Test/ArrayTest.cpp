8bc775945e871e9cf8723c4f47c4630839a2d8b6
n.zinoviev <zinoviev.sd@gmail.com>
2023-11-10 18:38:55 +0300

    Add tests for implicit ctor, element access and comparisons

--- Src/Array/Test/ArrayTest.cpp
+++ Src/Array/Test/ArrayTest.cpp
@@ -9,15 +9,85 @@
 #include "Array.hpp"
 
 
-TEST(ArrayConstructorTest, ImplicitCtor)
+TEST(ArrayTest, ImplicitCtor)
 {
    /*** Arrange ***/
-   // Containers::Array<int, 5> array{1, 2, 3, 4, 5};
+   const std::size_t size = 5;
+   int expected[size]{1, 2, 3, 4, 5};
 
    /*** Act ***/
+   Containers::Array<int, 5> array{1, 2, 3, 4, 5};
 
    /*** Assert ***/
+   EXPECT_EQ(memcmp(expected, array.data(), sizeof(int) * size), 0);
 }
+
+TEST(ArrayTest, RandomAccess)
+{
+   /*** Arrange ***/
+   const std::size_t size = 5;
+   std::string expected[size]{
+      "one",
+      "two",
+      "free",
+      "four",
+      "five"
+   };
+
+   /*** Act ***/
+   Containers::Array<std::string, size> array;
+   std::copy(std::begin(expected), std::end(expected), std::begin(array));
+
+   /*** Assert ***/
+   EXPECT_EQ(size, array.size());
+
+   for (std::size_t i = 0; i < array.size(); ++i)
+   {
+      EXPECT_EQ(expected[i], array[i]);
+      EXPECT_EQ(expected[i], array.at(i));
+   }
+
+   // Проверка генерации исключения при неправильном доступе.
+   EXPECT_THROW(array.at(size), std::out_of_range);
+}
+
+TEST(ArrayTest, FrontAndBackAccess)
+{
+   /*** Arrange ***/
+   const std::size_t size = 5;
+   std::string expected[size]{
+      "one",
+      "two",
+      "free",
+      "four",
+      "five"
+   };
+
+   /*** Act ***/
+   Containers::Array<std::string, size> array;
+   std::copy(std::begin(expected), std::end(expected), std::begin(array));
+
+   /*** Assert ***/
+   EXPECT_EQ(expected[0], array.front());
+   EXPECT_EQ(expected[size - 1], array.back());
+}
+
+
+TEST(ArrayTest, ComparisonOperators)
+{
+   /*** Arrange ***/
+   Containers::Array<int, 5> arr1{1, 2, 3, 4, 5};
+   Containers::Array<int, 5> arr2{1, 2, 3, 4, 5};
+   Containers::Array<int, 5> arr3{5, 4, 3, 2, 1};
+
+   /*** Act && Assert ***/
+   EXPECT_EQ(arr1, arr2);
+   EXPECT_NE(arr1, arr3);
+
+   EXPECT_GT(arr3, arr1);
+   EXPECT_LT(arr1, arr3);
+}
+
 
 
 ///////////////////////////////////////////////////////////////////////////////
